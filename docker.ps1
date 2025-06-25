3c6c81dd4192ec42ec9f1a1e3debfb5ff682d7e9
Marlon <marlonfda@gmail.com>
2025-06-25 15:01:19 -0300

    Project configuration to run with docker

---
+++ docker.ps1
@@ -0,0 +1,22 @@
+#Invoke app in container
+Write-Host "Running project in container ..."
+Write-Host "`nPress Ctrl + C to next step...`n"
+docker exec -it mcp_seriesgenerator_app dotnet McpSeriesGenerator.App.dll
+
+#List files for copy
+Write-Host "`nList files in /artifacts/ from container ..."
+Start-Sleep -s 5
+docker exec -it mcp_seriesgenerator_app ls ./artifacts/
+
+#Copy files...
+Write-Host "`nCopy artifacts files from container to /artifacts/docker/ ..."
+Start-Sleep -s 5
+$destination = ".\artifacts\docker"
+# Creates the destination folder if it does not exist
+if (-not (Test-Path $destination)) {
+    New-Item -ItemType Directory -Path $destination | Out-Null
+}
+docker cp mcp_seriesgenerator_app:/app/artifacts/. ./artifacts/docker
+
+Write-Host "`nFinished. Press Enter to exit..."
+Read-Host
