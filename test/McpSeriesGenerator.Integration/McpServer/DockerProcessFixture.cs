5553d6456645f97983b2dd836f6b41d7078b3690
Marlon <marlonfda@gmail.com>
2025-06-29 23:39:15 -0300

    Creating integrated tests with docker container and Mcp Server

---
+++ test/McpSeriesGenerator.Integration/McpServer/DockerProcessFixture.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+namespace McpSeriesGenerator.Integration.McpServer
+{
+    public class DockerProcessFixture : IDisposable
+    {
+        public Process _process { get; private set; }
+
+        public DockerProcessFixture()
+        {
+            _process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "docker",
+                    Arguments = "exec -i mcp_seriesgenerator_app bash -c \"dotnet McpSeriesGenerator.App.dll\"",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            _process.Start();
+            if (_process == null)
+            {
+                throw new InvalidOperationException("Failed to start MCP server process");
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_process != null && !_process.HasExited)
+            {
+                try
+                {
+                    _process.Kill();
+                    _process.Dispose();
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+
+            _process?.Dispose();
+        }
+    }
+}
---
+++ test/McpSeriesGenerator.Integration/McpServer/McpServerTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace McpSeriesGenerator.Integration.McpServer
+{
+    public class McpServerTest : IClassFixture<DockerProcessFixture>
+    {
+        private readonly DockerProcessFixture _fixture;
+
+        public McpServerTest(DockerProcessFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task MCPServer_ShouldResponseToListOfTools()
+        {
+            Assert.NotNull(_fixture._process);
+
+            _ = Task.Run(async () =>
+            {
+                string? err;
+                while ((err = await _fixture._process.StandardError.ReadLineAsync()) != null)
+                    Console.WriteLine("[STDERR] " + err);
+            });
+            var jsonString = JsonSerializer.Serialize(new
+            {
+                jsonrpc = "2.0",
+                id = 1,
+                method = "tools/list",
+                @params = new { }
+            });
+            await _fixture._process.StandardInput.WriteLineAsync(jsonString);
+            await _fixture._process.StandardInput.FlushAsync();
+            if (_fixture._process.StandardOutput.Peek() > 0)
+            {
+                var responseOut = await _fixture._process.StandardOutput.ReadLineAsync();
+                Assert.True(!string.IsNullOrWhiteSpace(responseOut));
+                Assert.Contains("jsonrpc", responseOut);
+                JsonNode? node = JsonNode.Parse(responseOut);
+                Assert.NotNull(node);
+                Assert.NotNull(node?["result"]);
+                Assert.NotNull(node?["result"]?["tools"]);
+            }
+        }
+    }
+}
